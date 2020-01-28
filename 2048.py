e6d102a1f53b97f9d0f2e9b53eed58e13bf989c8
Reza Primardiansyah <reza.primardiansyah@gmail.com>
2020-01-28 11:16:05 +0700

    add unit test. implement move left

--- 2048.py
+++ 2048.py
@@ -1,5 +1,7 @@
 from os import system, name
 
+import unittest
+
 def cls():
 	# stolen from screen_clear from https://www.tutorialspoint.com/how-to-clear-screen-using-python
    if name == 'nt':
@@ -9,8 +11,74 @@ def cls():
       _ = system('clear')
 
 
-dim = int(input('dimension = '))
-tgt = int(input('target score = '))
+def halo():
+	return 'halo'
 
-cls()
-print(dim, tgt)
+def init_board(n):
+	row = [0] * n
+	return [row] * n
+
+def print_board(board, dim):
+	for i in range(dim):
+		row = board[i]
+		print(''.join([' ' if j==0 else str(j) for j in row ]))
+
+
+def move_left(board):
+	d = len(board)
+	for row in board:
+		row = [j for j in row if j != 0]
+		row = row + [0]*(d-len(row))
+	return board
+
+
+def main():
+	dim = int(input('dimension = '))
+	tgt = int(input('target score = '))
+
+	cls()
+	board = init_board(dim)
+	print_board(board, dim)
+
+class TestBoard(unittest.TestCase):
+	def test_truth(self):
+		h = halo()
+		self.assertEqual('halo', h)
+
+
+	def test_board(self):
+		board = [
+		[2,0,0,0],
+		[0,2,0,0],
+		[0,0,2,0],
+		[0,0,0,2]
+		]
+		new_board = [
+		[2,0,0,0],
+		[0,2,0,0],
+		[0,0,2,0],
+		[0,0,0,2]
+		]
+		self.assertEqual(new_board, board)
+
+
+	def test_move_left(self):
+		board = [
+		[2,0,0,0],
+		[0,2,0,0],
+		[0,0,2,0],
+		[0,0,0,2]
+		]
+		new_board = [
+		[2,0,0,0],
+		[2,0,0,0],
+		[2,0,0,0],
+		[2,0,0,0],
+		]
+		new_board = move_left(board)
+		self.assertEqual(new_board, board)
+
+
+if __name__ == '__main__':
+	main()
+
